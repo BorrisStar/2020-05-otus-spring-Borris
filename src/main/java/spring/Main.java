669314f60ca4f16ea60178b037c9fd1e0be06eba
Стародубцев Борис Владимирович <Starodubtsev.B.V@sberbank.ru>
2020-07-05 12:17:27 +0300

    HomeWork 02+

--- src/main/java/spring/Main.java
+++ src/main/java/spring/Main.java
@@ -2,6 +2,8 @@ package spring;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
 import spring.service.ScannerService;
 import spring.service.StudentSurveyService;
 
@@ -9,8 +11,10 @@ import java.io.IOException;
 
 //Программа должна спросить у пользователя фамилию и имя, спросить 5 вопросов из CSV-файла и вывести результат тестирования.
 @ComponentScan
+@ShellComponent
 public class Main {
 
+    @ShellMethod(key = "start survey", value = "Start survey of students")
     public static void main(String[] args) throws IOException {
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
